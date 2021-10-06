1dbdfa192cc42be6910af666e372e32c3dc0a0c2
ujangaja <ujangaja@gmail.com>
2021-10-06 13:53:47 +0700

    penggunaan useState Hook pada react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
+import { useState } from "react";
 import Header from "./comopents/Header";
 
 function App() {
-  const title = "welcome to my App";
-  const age = 20;
-  const link = "https://mfikri.com";
-  const clickme = (name) => {
-    console.log("Hello " + name);
+  const [title, setTittle] = useState("welcome to my App");
+  const [age, setAge] = useState(20);
+  const changeTitle = () => {
+    setTittle("Title changed");
+    setAge(40);
   };
   return (
     <div className="App">
       <Header />
       <h1>{title}</h1>
-      <h1>{age * 2}</h1>
-      <h1>
-        <a href={link}>Go to google</a>
-      </h1>
-      <button onClick={() => clickme("Ujang")}>Click Me</button>
+      <h1>age : {age}</h1>
+      <button onClick={changeTitle}>Change title</button>
     </div>
   );
 }
