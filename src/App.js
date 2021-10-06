e6f4803460a212d36f1dd13645e0deffcd15e14f
ujangaja <ujangaja@gmail.com>
2021-10-06 13:30:55 +0700

    dynamic value

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
 function App() {
+  const title = "welcome to my App";
+  const age = 20;
+  const link = "https://mfikri.com";
   return (
     <div className="App">
-      <h1>Welcome</h1>
+      <h1>{title}</h1>
+      <h1>{age * 2}</h1>
+      <h1>
+        <a href={link}>Go to google</a>
+      </h1>
     </div>
   );
 }
